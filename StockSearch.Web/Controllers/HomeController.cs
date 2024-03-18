9778a7cafc8bae245b1b731fc797de6bf4eba1d5
hanifmaleki <hanif_1362@yahoo.com>
2024-03-18 14:44:15 +0100

    Initial Commit

---
+++ StockSearch.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using StockSearch.Core.Domain;
+using StockSearch.Web.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockSearch.Web.Controllers;
+
+public class HomeController: Controller 
+{
+    private static string API_URL = "https://ps-async.fekberg.com/api/stocks";
+
+    public async Task<IActionResult> Index()
+    {
+        using (var client = new HttpClient())
+        {
+            var responseTask = client.GetAsync($"{API_URL}/MSFT");
+
+            var response = await responseTask;
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+
+            return View(data);
+        }
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
